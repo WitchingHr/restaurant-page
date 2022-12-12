9a1f1d2d77acd58223d46d041d7f1405715c280e
WitchingHr <matt1494@gmail.com>
2022-12-11 23:19:54 -0500

    Create file structure

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}

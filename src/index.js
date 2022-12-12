76995026622678c5a197e6c1e44067ef386006c2
WitchingHr <matt1494@gmail.com>
2022-12-12 17:17:45 -0500

    Add placeholder logo

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ const header = document.createElement('div');
 header.classList.add('header');
 content.appendChild(header);
 
+const ribbon = document.createElement('div');
+ribbon.classList.add('ribbon');
+header.appendChild(ribbon);
+
+const logo = document.createElement('div');
+logo.classList.add('logo');
+logo.innerHTML = 'Juicy Chicken';
+header.appendChild(logo);
+
+
 const main = document.createElement('div');
 main.classList.add('main');
 
 const footer = document.createElement('div');
 footer.classList.add('footer');
-
-
-
-

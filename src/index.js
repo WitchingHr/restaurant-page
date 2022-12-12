f5eef00e9337b6ebf97da7d313e5b0b728098359
WitchingHr <matt1494@gmail.com>
2022-12-12 15:37:53 -0500

    Bundle css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-console.log('Hello World!');
+import css from './styles/style.css';
 
-console.log('hi');
+const content = document.querySelector('.content');
+
+const header = document.createElement('div');
+header.classList.add('header');
+content.appendChild(header);
+
+const main = document.createElement('div');
+main.classList.add('main');
+
+const footer = document.createElement('div');
+footer.classList.add('footer');
+
+
+
+

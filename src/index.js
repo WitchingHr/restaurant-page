9331ca81d988962e84d97800b67bfde776f6722e
WitchingHr <matt1494@gmail.com>
2022-12-13 11:04:59 -0500

    Add logo and navbar

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import css from './styles/style.css';
 
+// Content wrapper
 const content = document.querySelector('.content');
 
 const header = document.createElement('div');
 header.classList.add('header');
 content.appendChild(header);
 
-const ribbon = document.createElement('div');
-ribbon.classList.add('ribbon');
-header.appendChild(ribbon);
-
 const logo = document.createElement('div');
 logo.classList.add('logo');
-logo.innerHTML = 'Juicy Chicken';
 header.appendChild(logo);
 
+// Navbar
+const nav = document.createElement('div');
+nav.classList.add('nav');
+content.appendChild(nav);
 
+const menuBtn = document.createElement('button');
+menuBtn.innerHTML = 'MENU';
+menuBtn.classList.add('navBtn');
+nav.appendChild(menuBtn);
+
+const contactBtn = document.createElement('button');
+contactBtn.innerHTML = 'CONTACT';
+contactBtn.classList.add('navBtn');
+nav.appendChild(contactBtn);
+
+// Main
 const main = document.createElement('div');
 main.classList.add('main');
 
+// Footer
 const footer = document.createElement('div');
 footer.classList.add('footer');

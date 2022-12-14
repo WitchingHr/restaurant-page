f3d7263b0c365acf352cc944a807d3e7971d2c1d
WitchingHr <matt1494@gmail.com>
2022-12-13 22:50:50 -0500

    Layout footer links in grid

--- src/footer.js
+++ src/footer.js
@@ -2,13 +2,17 @@ import Insta from './icons/instagram.png';
 import Fb from './icons/facebook.png';
 import Twitter from './icons/twitter.png';
 
-export default function printFooter() {
+export default function printwrapper() {
   const footer = document.querySelector('.footer');
+
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('footer-wrapper');
+  footer.appendChild(wrapper);
 
   // List
   const links = document.createElement('ul');
   links.classList.add('footer-links');
-  footer.appendChild(links);
+  wrapper.appendChild(links);
 
   const contactLi = document.createElement('li');
   links.appendChild(contactLi);
@@ -26,10 +30,26 @@ export default function printFooter() {
   careers.innerHTML = 'CAREERS';
   careersLi.appendChild(careers);
 
+  const giftLi = document.createElement('li');
+  links.appendChild(giftLi);
+
+  const giftCards = document.createElement('a');
+  giftCards.setAttribute('href', '');
+  giftCards.innerHTML = 'GIFT CARDS';
+  giftLi.appendChild(giftCards);
+
+  const fundLi = document.createElement('li');
+  links.appendChild(fundLi);
+
+  const fundraising = document.createElement('a');
+  fundraising.setAttribute('href', '');
+  fundraising.innerHTML = 'FUNDRAISING';
+  fundLi.appendChild(fundraising);
+
   // Social
   const social = document.createElement('div');
   social.classList.add('social');
-  footer.appendChild(social);
+  wrapper.appendChild(social);
 
   const connect = document.createElement('div');
   connect.classList.add('connect');
